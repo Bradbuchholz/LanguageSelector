432b6dbe94a01ef4a903b5c230495d4451dfcf64
Bradbuchholz <bradbuchholz@gmail.com>
2022-04-09 18:56:11 -0700

    Add form id and inputs to index.html, add const in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(funtion() {
+  $("form#programming").submit(function(event) {
+    const age = parseInt($("input#age").valueOf());
+    const experience = $("select#experience").val();
+    const pets = $("select#pets").val();
+    const seasons = $("select#seasons").val();
+    const flavor = $("select#flavor").val();
+
+    if 
+  }
+}
