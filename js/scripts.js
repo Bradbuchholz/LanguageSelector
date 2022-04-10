ed529e1a362b4cdd6938a491f1d50ea86d7dcbdb
Bradbuchholz <bradbuchholz@gmail.com>
2022-04-09 20:37:42 -0700

    Add else if and else to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(funtion() {
   $("form#programming").submit(function(event) {
+    event.preventDefault();
     const age = parseInt($("input#age").valueOf());
     const experience = $("select#experience").val();
     const pets = $("select#pets").val();
     const seasons = $("select#seasons").val();
     const flavor = $("select#flavor").val();
 
-    if (age >=40)
+    if (age >=40 && experience === "no" && pets === "cat" && seasons === "spring" && flavor === "vanilla") {
+      $('.program-option').addClass('display-none');
+      $('#ruby').removeClass('display-none');
+    }
   }
+
 }
